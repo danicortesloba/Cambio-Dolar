098ada952f5084fc73dc66a6191424af12992799
dani <daniela.cortes@thoughtworks.com>
2020-03-18 18:42:39 -0300

    Able to update past dollar

--- exchange.js
+++ exchange.js
@@ -1,12 +1,13 @@
 const fetch = require('node-fetch');
-var currentDolar;
-var pastDolar = 0;
+
+
 function getCurrency() {
   fetch('https://mindicador.cl/api')
   .then(response => response.json())
   .then(response => {
-      date = response.dolar.fecha
-    	currentDolar = response.dolar.valor;
+      var pastDolar;
+      var date = response.dolar.fecha;
+    	var currentDolar = response.dolar.valor;
       if(currentDolar != pastDolar){
         console.log(currentDolar, date )
       }
