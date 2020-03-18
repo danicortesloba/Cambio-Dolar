17e31b8513d2edcc244e2413017ce69d99cf37ff
dani <daniela.cortes@thoughtworks.com>
2020-03-18 18:35:06 -0300

    Print current dolar and date if there is a change.

--- exchange.js
+++ exchange.js
@@ -1,10 +1,16 @@
 const fetch = require('node-fetch');
 var currentDolar;
+var pastDolar = 0;
 function getCurrency() {
   fetch('https://mindicador.cl/api')
   .then(response => response.json())
   .then(response => {
-    	console.log(response.dolar.valor);
+      date = response.dolar.fecha
+    	currentDolar = response.dolar.valor;
+      if(currentDolar != pastDolar){
+        console.log(currentDolar, date )
+      }
+      pastDolar = currentDolar;
   })
   .catch(err => "ERROR");
 }
