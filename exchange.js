e333a5a7d887ee7da618f4b96b59c3b722d76a18
dani <daniela.cortes@thoughtworks.com>
2020-03-18 18:21:48 -0300

    Log value of dolar

--- exchange.js
+++ exchange.js
@@ -4,19 +4,13 @@ function getCurrency() {
   fetch('https://mindicador.cl/api')
   .then(response => response.json())
   .then(response => {
-    parseResponse(response);
+    	console.log(response.dolar.valor);
   })
   .catch(err => "ERROR");
 }
 
-function parseResponse(response){
-  var parsed = JSON.parse(response);
-  console.log(parsed.count);
-}
 
-//function getDivisaByCode(codigo, response) {
-  //console.log(codigo, response);
-//}
+
 
 
 getCurrency();
