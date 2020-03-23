1d433e5d46b4cf2847bb69c2f917aa3ae32722bf
dani <daniela.cortes@thoughtworks.com>
2020-03-23 17:38:59 -0300

    Fix user model

--- bot.js
+++ bot.js
@@ -10,6 +10,7 @@ const command = /\/get_dolar/;
 telegram.on(command, (message) => {
     if (command) {
         telegram.sendMessage(message.chat.id, getCurrentDolar());
+        console.log(message);
     } else {
         telegram.sendMessage(message.chat.id, "Pídeme el dolar");
     }
--- data/db/db.js
+++ data/db/db.js
@@ -1,17 +1,20 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
 
-var db = mongoose.connection;
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
 
 });
 
-var userSchema = new mongoose.Schema({
-  name: String,
-  telephone: Number
+const userSchema = new mongoose.Schema({
+  id: Number,
+  is_bot: Boolean,
+  first_name: String,
+  language_code: String
 });
-var User = mongoose.model('User', userSchema);
 
-var chano = new User({ name: 'Chano', telephone: 12345 });
-console.log(chano.name);
+const User = mongoose.model('User', userSchema);
+
+const chano = new User({ id:1234, is_bot: false, first_name: 'Chano', language_code: 'es'});
+console.log(chano.is_bot);
