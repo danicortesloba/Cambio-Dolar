46447caecbc32022f56e51a967b4e4d436ece18e
dani <daniela.cortes@thoughtworks.com>
2020-03-27 11:26:04 -0300

    Fix findOne in subscription status

--- bot.js
+++ bot.js
@@ -1,12 +1,10 @@
 const { token } = require('./config');
 const { getCurrentDolar } = require('./exchange');
 const User = require("./data/db/db");
-
-
 const TeleBot = require('telebot');
 const telegram = new TeleBot(token)
-
 const fetch = require('node-fetch');
+
 const get_dolar = /\/get_dolar/;
 const subscribe = /\/subscribe/;
 const unsubscribe = /\/unsubscribe/;
@@ -24,26 +22,22 @@ async function updateSubscriptionTrue(message){
   const update = { subscription: true };
   let doc = await User.findOneAndUpdate(filter, update);
   telegram.sendMessage(message.chat.id, "You are subscribed!");
-
 }
 
 function giveSubscriptionStatus(user, message){
-
-  console.log(user.subscription);
-  //if(user.subscription){
-  //telegram.sendMessage(message.chat.id, "You are subscribed");
-  //} else {
-  //telegram.sendMessage(message.chat.id, "You are not subscribed");
-  //}
+  if(user.subscription){
+  telegram.sendMessage(message.chat.id, "You are subscribed");
+  } else {
+  telegram.sendMessage(message.chat.id, "You are not subscribed");
+  }
 }
 
 telegram.on(get_dolar, (message) => {
     if (get_dolar) {
       const callback = (err, res) => console.log("Error: ", err, "Result: ", res)
       telegram.sendMessage(message.chat.id, getCurrentDolar());
-   }
+    }
 });
-
 
 telegram.on(subscribe, (message) => {
     if (subscribe && message.chat.type == "private"){
@@ -54,33 +48,28 @@ telegram.on(subscribe, (message) => {
             let user = new User({id: message.from.id, is_bot: message.from.is_bot, first_name: message.from.first_name, language_code: message.from.language_code, subscription: true})
             user.save();
             telegram.sendMessage(message.chat.id, "You are subscribed!");
-
           }
-
       });
-
     }
 });
 
 telegram.on(unsubscribe, (message) => {
     if (unsubscribe){
       updateSubscriptionFalse(message);
-      telegram.sendMessage(message.chat.id, "You are unsubscribed!")    }
+      telegram.sendMessage(message.chat.id, "You are unsubscribed!")
+    }
 });
 
 telegram.on(subscription_status, (message) => {
     if (subscription_status){
-
-      User.find({id: message.from.id}, function (err, user) {
+      User.findOne({id: message.from.id}, function (err, user) {
           if(err){
             return console.log(err);
           } else {
             giveSubscriptionStatus(user, message);
           }
-
       });
     }
 });
-
 
 telegram.start();
