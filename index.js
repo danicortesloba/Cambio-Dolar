271aa0c6b79e14a12cba6ce0c1372457f75b3d71
dani <daniela.cortes@thoughtworks.com>
2020-04-06 16:30:10 -0400

    Wake up dyno

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const telegram = require('./bot');
 const app = require('./api');
 const task = require('./cron');
 const mongoose = require('mongoose');
-const wakeUpDyno = require("wokeDyno.js");
+const wakeUpDyno = require("woke-dyno.js");
 
 mongoose.connect(process.env.CONECTION_STRING, {useNewUrlParser: true});
 const db = mongoose.connection;
