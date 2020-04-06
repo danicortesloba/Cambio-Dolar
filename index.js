cd1f29df98457948294c4988ac780b53631aba4f
dani <daniela.cortes@thoughtworks.com>
2020-04-06 16:28:15 -0400

    Wake up dyno

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 require('dotenv').config()
 const telegram = require('./bot');
 const app = require('./api');
-const task = require('./cron')
+const task = require('./cron');
 const mongoose = require('mongoose');
+const wakeUpDyno = require("wokeDyno.js");
 
 mongoose.connect(process.env.CONECTION_STRING, {useNewUrlParser: true});
 const db = mongoose.connection;
@@ -16,4 +17,6 @@ telegram.start();
 
 task.start();
 
-app.listen(process.env.PORT, () => console.log(`App listening on port ${process.env.PORT}!`))
+app.listen(process.env.PORT, () =>  {
+    wakeUpDyno(process.env.CONECTION_STRING);
+})
